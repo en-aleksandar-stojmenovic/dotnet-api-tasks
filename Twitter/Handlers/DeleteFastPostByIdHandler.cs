3d5e9ba600e915eb96773307ce13f15919226452
Aleksandar <aleksandar.stojmenovic@endava.com>
2022-12-28 15:51:20 +0100

    Added DeleteFastPost endpoint

---
+++ Twitter/Handlers/DeleteFastPostByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Twitter.Queries;
+using Twitter.Repositories.Interfaces;
+
+namespace Twitter.Handlers
+{
+	public class DeleteFastPostByIdHandler : IRequestHandler<DeleteFastPostByIdQuery, bool>
+	{
+		private readonly IRepositoryWrapper _repository;
+
+		public DeleteFastPostByIdHandler(IRepositoryWrapper repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<bool> Handle(DeleteFastPostByIdQuery request, CancellationToken cancellationToken)
+		{
+			_repository.FastPost.DeleteFastPost(request.Id);
+
+			var deleted = await _repository.SaveAsync();
+
+			return deleted > 0;
+		}
+	}
+}
---
+++ Twitter/Queries/DeleteFastPostByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace Twitter.Queries
+{
+	public class DeleteFastPostByIdQuery : IRequest<bool>
+	{
+		public Guid Id { get; set; }
+
+		public DeleteFastPostByIdQuery(Guid id)
+		{
+			Id = id;
+		}
+	}
+}
