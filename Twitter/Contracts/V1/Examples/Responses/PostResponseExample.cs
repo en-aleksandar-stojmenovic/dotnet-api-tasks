d3a0e35e97968071a72946de5df665b3e81c0091
Aleksandar <aleksandar.stojmenovic@endava.com>
2022-12-30 14:58:34 +0100

    Extended documentation for Post endpoints

---
+++ Twitter/Contracts/V1/Examples/Responses/PostResponseExample.cs
@@ -0,0 +1,22 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System;
+using Twitter.Domain;
+
+namespace Twitter.Contracts.V1.Examples.Responses
+{
+	public class PostResponseExample : IExamplesProvider<Post>
+	{
+		public Post GetExamples()
+		{
+			return new Post
+			{
+				Id = Guid.NewGuid(),
+				Text = "Some text",
+				Created = DateTime.Now,
+				CategoryId = Guid.NewGuid(),
+				CreatedBy = Guid.NewGuid(),
+			};
+		}
+	}
+}
+
