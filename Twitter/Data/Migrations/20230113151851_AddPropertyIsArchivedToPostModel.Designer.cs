7349e528012b4519debd242533e4504134ba2e96
Aleksandar <aleksandar.stojmenovic@endava.com>
2023-01-13 16:31:47 +0100

    Add property IsArchived to Post model

--- Twitter/Data/Migrations/20230113151851_AddPropertyIsArchivedToPostModel.Designer.cs
+++ Twitter/Data/Migrations/20230113151851_AddPropertyIsArchivedToPostModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Twitter.Data;
 
@@ -11,9 +12,10 @@ using Twitter.Data;
 namespace Twitter.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230113151851_AddPropertyIsArchivedToPostModel")]
+    partial class AddPropertyIsArchivedToPostModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
